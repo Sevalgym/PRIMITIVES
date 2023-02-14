7afec073d1ca39d69bd285949a34e343c46ecf66
Anton <sevalnikov.ant@gmail.com>
2023-02-14 16:19:46 +0300

    primitiveLineMiles

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        int account = 20;
-        int deposit = 10000;
-
-        int bonus;
-        if (deposit > 20) {
-            bonus = deposit / 20;
-        } else {
-            bonus = 0;
-        }
-        int totalAmount = deposit;
-
-        System.out.print("Сумма стоимости билета: ");
-        System.out.print(totalAmount);
-        System.out.println("P");
-        System.out.print("Бесплатные мили за перелёты: ");
-        System.out.print(bonus);
-        System.out.println("P");
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
